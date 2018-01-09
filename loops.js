652b47a1def786455be3bbd0da3ef3d7ed4e6569
First Last <flatironcamp.tc99@mamber.net>
2018-01-09 04:13:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -20,9 +20,12 @@ function whileLoop(num) {
 function doWhileLoop(arrayInput) {
   do {
     // specs didn't say remove from front or back; did back
+    
     // fails silently (returns undefined) if empty array, so execute even
     // if array initially empty, but test array at condition to stop on 
     // empty array.  
     array.Input.pop(); 
   } while ((Math.random() >= 0.5) && (array.length));
+  
+  return arrayInput;
 }
