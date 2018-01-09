b629c7d319417752e02472ee5812fca9683622a6
First Last <flatironcamp.tc99@mamber.net>
2018-01-09 03:48:18 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,6 @@
 function forLoop(arrayInput) {
   // var newArray = arrayInput;
   
-  console.log("before: length is " + arrayInput.length);
   for (var i=0; i<25; i++) {
     if (i === 1) {
       arrayInput.push("I am 1 strange loop.");
